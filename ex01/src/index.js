e8dfce1545c78a40117681947d4102e1821c0981
Mirela <adisserdarevic@gmail.com>
2021-03-17 20:43:44 +0100

    Added file index.js

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -2,18 +2,13 @@ var myPet = {
     species: "Cat",
     name: "Cicko",
     legs: "4",
-    friends: "Fifi, Dzeki"
-};
+    friends: ["Fifi, Dzeki"]
+}
 function myFunction(myObj) {
-    // Only change code below this line
-     
+    // Only change code below this line    
     return myObj;
-    
     // Only change code below this line
-    
 }
 console.log(myFunction(myPet));
-
-
 
 module.exports = {myPet, myFunction};
