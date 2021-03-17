202e2be3f540dc3056b31f9d0b934052975a0495
Mirela <adisserdarevic@gmail.com>
2021-03-17 22:04:29 +0100

    Added file index.js

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -4,8 +4,10 @@ var school = {
     established: 2020
 }
 function myFunction(name) {
+    // Only change code below this line
     school.name = "Paragon";
     return school;
+    // Only change code above this line
 }
 myFunction("Paragon");
 console.log(myFunction(school.name));
