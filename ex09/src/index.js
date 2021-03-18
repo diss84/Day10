3640de062a028fe18d29a825a5cf6a1257d9f41b
Mirela <adisserdarevic@gmail.com>
2021-03-18 07:18:50 +0100

    Added file index.js

---
+++ ex09/src/index.js
@@ -0,0 +1,21 @@
+var checkProp = {
+    title: "Titanic",
+    song: "My Heart Will Go On",
+    genre: "drama",
+    actor: "Not Found"
+};
+function myFunction(myObj) {
+    // Only change code below this line
+    if (checkProp.title){
+        return "Titanic";
+    }        
+    
+    // Only change code above this line
+    
+
+}
+
+myFunction();
+console.log(myFunction());
+
+module.exports = myFunction;
